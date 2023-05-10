d44fc5df9f5dda98f64f267144037984c8efe660
syakira09 <114804271+syakira09@users.noreply.github.com>
2023-05-11 00:00:33 +0700

    Update sslmar.sh

--- sslmar.sh
+++ sslmar.sh
@@ -36,9 +36,9 @@ sleep 0.5
 sudo apt install nginx -y
 cd
 curl -fsSL https://get.docker.com | sh
-#wget -q -O /etc/nginx/conf.d/xray.conf "https://raw.githubusercontent.com/syakira09/MarXray-Dug-V1.1/master/xray.conf"
+#wget -q -O /etc/nginx/conf.d/xray.conf "https://raw.githubusercontent.com/syakira09/Marzban-Dug-V1.2/main/xray.conf"
 #sed -i 's/kiko8.pisces.my.id/${domain}/g' /etc/nginx/conf.d/xray.conf
-wget -q -O tesidom.sh https://raw.githubusercontent.com/syakira09/Marzban-Dug-V1.1/main/tesidom.sh && chmod +x tesidom.sh && ./tesidom.sh
+wget -q -O tesidom.sh https://raw.githubusercontent.com/syakira09/Marzban-Dug-V1.2/main/tesidom.sh && chmod +x tesidom.sh && ./tesidom.sh
 sleep 1
 rm -r /etc/nginx/nginx.conf
 wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/amarukael/config/main/nginx.conf"
@@ -46,7 +46,7 @@ service nginx restart
 cd
 wget -qO- https://surgahostinganaswae.com/scrip-chr/marzban/multi-port/v1.1/multi-portv1.1.tar.gz | tar xz --xform 's/multi-portv1.1/marzban/' && cd marzban
 rm -r xray_config.json
-wget -q -O /root/marzban/xray_config.json "https://raw.githubusercontent.com/amarukael/configig/main/config.json"
+wget -q -O /root/marzban/xray_config.json "https://raw.githubusercontent.com/amarukael/config/main/config.json"
 docker compose up -d
 clear 
 echo "Install telah selesai"
